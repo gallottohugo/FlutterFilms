8b976d0197c539e12ac7ca1e981ea5721b03220f
gallottohugo <gallottohugo@gmail.com>
2019-12-30 18:19:27 -0300

    Install flutter_swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 
 class HomePage extends StatelessWidget {
   	@override
@@ -6,11 +8,41 @@ class HomePage extends StatelessWidget {
     	return Scaffold(
 			
 			appBar: AppBar(
-				title: Text('Films'),
+				centerTitle: false,
+				title: Text('Movies in cinema'),
+				backgroundColor: Colors.indigoAccent,
+				actions: <Widget>[
+					IconButton(
+						icon: Icon(Icons.search),
+						onPressed: (){},
+					)
+				],
 			),
-      		body: SafeArea(
-				  child: Text('Films home page'),
-			  ),
+      		body: Container(
+				  child: Column(
+					  children: <Widget>[
+						  _swiperCards()
+					  ],
+				  ),
+			  )
     	);
   	}
+
+	Widget _swiperCards(){
+		return Container(
+			padding: EdgeInsets.only(top: 10.0),
+			width: double.infinity,
+			height: 300.0,
+		  	child: Swiper(
+				layout: SwiperLayout.STACK,
+				itemWidth: 300.0,
+        		itemBuilder: (BuildContext context,int index){
+          			return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+        		},
+        		itemCount: 3,
+        		//pagination: new SwiperPagination(),
+        		//control: new SwiperControl(),
+      		),
+		);
+	}
 }
