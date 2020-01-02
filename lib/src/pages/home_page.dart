b70d293670eba8fd9942d42ed685e0db86baa210
gallottohugo <gallottohugo@gmail.com>
2020-01-02 15:20:54 -0300

    Add swiper horizontal to home page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:films/src/providers/movies_provider.dart';
+import 'package:films/src/widgets/card_swiper_horizontal_widget.dart';
 import 'package:films/src/widgets/card_swiper_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -23,9 +24,11 @@ class HomePage extends StatelessWidget {
 				],
 			),
       		body: Container(
-				  child: Column(
-					  children: <Widget>[
-						  _swiperCards()
+				child: Column(
+					children: <Widget>[
+						_swiperCards(),
+						Divider(),
+						_swiperFooter(context)
 					  ],
 				  ),
 			  )
@@ -33,7 +36,6 @@ class HomePage extends StatelessWidget {
   	}
 
 	Widget _swiperCards(){
-		
 		return FutureBuilder(
 		  	future: moviesProvier.getNowPlaying(),
 		  	builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
@@ -48,8 +50,36 @@ class HomePage extends StatelessWidget {
 					  	),
 					);
 				}
-				
 		  	},
+		);
+	}
+
+	Widget _swiperFooter(BuildContext context){
+		return Container(
+			padding: EdgeInsets.only(top: 10.0),
+			width: double.infinity,
+			child: Column(
+				crossAxisAlignment: CrossAxisAlignment.start,
+				children: <Widget>[
+					Container(
+						padding: EdgeInsets.all(10.0),
+						child: Text('Popular movies', style: Theme.of(context).textTheme.subtitle,)
+					),
+					SizedBox(height: 5.0,),
+					FutureBuilder(
+					  future: moviesProvier.getPopulars(),
+					  builder: (BuildContext context, AsyncSnapshot snapshot) {
+						if (snapshot.hasData){
+							return CardSwiperHorizontalWidget(movies: snapshot.data,);
+						}
+						else{
+							return Center(child: CircularProgressIndicator());
+								
+						}
+					  },
+					),
+				],
+			),
 		);
 	}
 }
