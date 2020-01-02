f8ad14b1304b8d2b52636a0303d401063bf8a018
gallottohugo <gallottohugo@gmail.com>
2020-01-02 12:05:53 -0300

    Refactoring Card Swiper

--- lib/src/widgets/card_swiper_widget.dart
+++ lib/src/widgets/card_swiper_widget.dart
@@ -1,10 +1,11 @@
+import 'package:films/src/models/movie_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
 
 class CardSwiperWidget extends StatelessWidget {
 
-	final List<dynamic> movies;
+	final List<Movie> movies;
 	
 	CardSwiperWidget({@required this.movies});
 
@@ -17,20 +18,23 @@ class CardSwiperWidget extends StatelessWidget {
 
     	return Container(
 			padding: EdgeInsets.only(top: 20.0),
-		  	child: Swiper(
+		  	child: Swiper(				  
 				itemWidth: _screenSize.width * 0.7,
 				itemHeight: _screenSize.height * 0.5,
 				layout: SwiperLayout.STACK,
 				itemCount: movies.length,
 				//pagination: new SwiperPagination(),
         		//control: new SwiperControl(),
-        		itemBuilder: (BuildContext context,int index){
+        		itemBuilder: (BuildContext context, int index){
+
+					String url_img = movies[index].getPosterImg();
           			return ClipRRect(
 						borderRadius: BorderRadius.circular(20),
-						child: Image.network(
-                            "http://via.placeholder.com/350x150",
-                            fit: BoxFit.cover,
-                        ),
+						child: FadeInImage(
+							image: NetworkImage(url_img),
+							placeholder: AssetImage('assets/img/no-image.jpg'),
+							fit: BoxFit.cover
+						)
 					);
         		},
       		),
