4ee0f023279c39fa625d191549ecff1ab4b62e23
gallottohugo <gallottohugo@gmail.com>
2020-01-02 10:57:00 -0300

    Create movide provider to connecto to the server

---
+++ lib/src/providers/movies_provider.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:films/src/models/movie_model.dart';
+import 'package:http/http.dart' as http;
+
+class MoviesProvier{
+    String _apiKey = 'e799781b8e4bccca31d1eb4462d736ad';
+  	String _url = 'api.themoviedb.org';
+  	String _language = 'en-US';
+
+	Future<List<Movie>> getNowPlaying() async {
+		final url = Uri.https(_url, '3/movie/now_playing', {
+			'api_key': _apiKey,
+			'language': _language
+		});
+
+		final response = await http.get(url);
+
+		final decodedData = json.decode(response.body);
+
+		final movies = Movies.fromJsonList(decodedData['results']);
+		return movies.listMovies;
+	}
+}
