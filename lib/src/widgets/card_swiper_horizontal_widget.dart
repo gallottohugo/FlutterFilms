a2de0fbae2dbd1a39f7ca59ada1f1b6bcfcb6a4a
gallottohugo <gallottohugo@gmail.com>
2020-01-03 11:07:13 -0300

    Add a PageView.builder and change childer of pageview to itembuilder

--- lib/src/widgets/card_swiper_horizontal_widget.dart
+++ lib/src/widgets/card_swiper_horizontal_widget.dart
@@ -8,7 +8,6 @@ class CardSwiperHorizontalWidget extends StatelessWidget {
   	CardSwiperHorizontalWidget({@required this.movies, @required this.nextPage});
 
 
-
 	final _pageController = new PageController(initialPage: 1, viewportFraction: 0.3);
 
 
@@ -17,49 +16,52 @@ class CardSwiperHorizontalWidget extends StatelessWidget {
 		final _screenSize = MediaQuery.of(context).size;
 		_pageController.addListener((){
 			if (_pageController.position.pixels >= _pageController.position.maxScrollExtent){
+				//Función declarada en el home page
 				nextPage();
 			}
 		});
 
     	return Container(
 			height: _screenSize.height * 0.25,
-			child: PageView(
+			child: PageView.builder(
 				pageSnapping: false,
 				controller: _pageController,
-				children: _cards(context),
+				itemCount: movies.length,
+				itemBuilder: (context, i){
+					return _card(context, movies[i]);
+				},
+
 				
 			),
     	);
   	}
 
 
-	List<Widget> _cards(BuildContext context){
-		return movies.map((movie){
-			return Container(
-				margin: EdgeInsets.only(right: 15.0),
-				child: Column(
-					children: <Widget>[
-						ClipRRect(
-							borderRadius: BorderRadius.circular(20.0),
-							child: FadeInImage(
-								image: NetworkImage(movie.getPosterImg()),
-								placeholder: AssetImage('assets/img/no-image.jpg'),
-								fit: BoxFit.cover,
-								height: 160.0,
-								width: 100.0,
-							),
-						), 
-						SizedBox(height: 5.0,),
-						Center(
-							child: Text(
-								movie.title, 
-								overflow: TextOverflow.ellipsis,
-								style: Theme.of(context).textTheme.caption,
-							),
-						)
-					],
-				),
-			);
-		}).toList();
+	Widget _card(BuildContext context, Movie movie){
+		return Container(
+			margin: EdgeInsets.only(right: 15.0),
+			child: Column(
+				children: <Widget>[
+					ClipRRect(
+						borderRadius: BorderRadius.circular(20.0),
+						child: FadeInImage(
+							image: NetworkImage(movie.getPosterImg()),
+							placeholder: AssetImage('assets/img/no-image.jpg'),
+							fit: BoxFit.cover,
+							height: 160.0,
+							width: 100.0,
+						),
+					), 
+					SizedBox(height: 5.0,),
+					Center(
+						child: Text(
+							movie.title, 
+							overflow: TextOverflow.ellipsis,
+							style: Theme.of(context).textTheme.caption,
+						),
+					)
+				],
+			),
+		);
 	}
 }
