92f8eeb2ad6f173cd55f1b99c58b7a2cad604ae6
gallottohugo <gallottohugo@gmail.com>
2020-01-03 10:24:46 -0300

    Create StreamBuilder - Sink - Stream

--- lib/src/widgets/card_swiper_horizontal_widget.dart
+++ lib/src/widgets/card_swiper_horizontal_widget.dart
@@ -3,21 +3,29 @@ import 'package:flutter/material.dart';
 
 class CardSwiperHorizontalWidget extends StatelessWidget {
   	final List<Movie> movies;
+	final Function nextPage;
 
-  	CardSwiperHorizontalWidget({@required this.movies});
+  	CardSwiperHorizontalWidget({@required this.movies, @required this.nextPage});
+
+
+
+	final _pageController = new PageController(initialPage: 1, viewportFraction: 0.3);
 
 
   	@override
   	Widget build(BuildContext context) {
 		final _screenSize = MediaQuery.of(context).size;
+		_pageController.addListener((){
+			if (_pageController.position.pixels >= _pageController.position.maxScrollExtent){
+				nextPage();
+			}
+		});
+
     	return Container(
 			height: _screenSize.height * 0.25,
 			child: PageView(
 				pageSnapping: false,
-				controller: PageController(
-					initialPage: 1,
-					viewportFraction: 0.3	
-				),
+				controller: _pageController,
 				children: _cards(context),
 				
 			),
