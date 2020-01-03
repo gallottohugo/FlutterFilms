250e52dfd7c00f1e40a64022dd0925a894f2ec2c
gallottohugo <gallottohugo@gmail.com>
2020-01-03 10:25:37 -0300

    Create StreamController - Sink - Stream

--- lib/src/providers/movies_provider.dart
+++ lib/src/providers/movies_provider.dart
@@ -1,11 +1,28 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:films/src/models/movie_model.dart';
 import 'package:http/http.dart' as http;
 
 class MoviesProvier{
+	String _url = 'api.themoviedb.org';
     String _apiKey = 'e799781b8e4bccca31d1eb4462d736ad';
-  	String _url = 'api.themoviedb.org';
   	String _language = 'en-US';
+	int _page = 0;
+
+
+	//STREAM
+	List<Movie> _popular_movies = new List();
+	final _streamController = StreamController<List<Movie>>.broadcast();
+	Function(List<Movie>) get popularSink => _streamController.sink.add;
+	Stream<List<Movie>> get popularStream  => _streamController.stream;
+
+	void diosposeStreams(){
+		_streamController?.close();
+	}
+
+
+
+	
 
 	Future<List<Movie>> getResponse(Uri url) async{
 		final response = await http.get(url);
@@ -22,11 +39,17 @@ class MoviesProvier{
 		return getResponse(url);
 	}
 
-  	Future<List<Movie>> getPopulars() async {
+	void getPopulars() async {
+		_page ++;
+
 		final url = Uri.https(_url, '3/movie/popular', {
-			'api_key': _apiKey,
-			'language': _language
+			'api_key' : _apiKey,
+			'language': _language,
+			'page'    : _page.toString()
 		});
-		return getResponse(url);
+
+		final response = await getResponse(url);
+		_popular_movies.addAll(response);
+		popularSink(_popular_movies);
 	}
 }
